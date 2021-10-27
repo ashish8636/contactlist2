ca87df408ebbd5045e6bed4afd6f5cd2b1b27998
ashish8636g <86728402+ashish8636g@users.noreply.github.com>
2021-10-27 18:54:59 +0530

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="hello"  >
+    <div class="col-md-12" v-for="item in list.data" v-bind:key="item.id">
+      <div class="row">
+        
+        <div class="col-md-2">
+           {{item.id}}
+        </div>
+        <div class="col-md-2">  
+          {{item.first_name}}
+        </div>
+          <div class="col-md-2">
+         {{item.last_name}}
+          </div>
+          <div class="col-md-2">
+        {{item.email}}
+          </div>
+        <div class="col-md-2">
+          <img :src="(item.avatar)">
+         
+        </div>
+        <div class="col-md-2" v-if="item!=null">
+          <button class="btn-primary" v-on:click="deletedatalist(item.id)"> Delete </button>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+
+export default {
+  data:()=>({
+    list:'',
+  }),
+  name: 'HelloWorld',
+  
+  created() {
+    
+  },
+  mounted(){
+   axios.get("https://reqres.in/api/users")
+    .then((result) => {this.list = result.data;
+    })
+  },
+  method:{
+    
+
+    deletedatalist(item){
+     axios.delete("https://reqres.in/api/users/" +item)
+          .then(( ) => {this.getdata();
+    })
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
